50557d3379aada7fee5100acb505c6c8289a92c1
Muthukrthika <muthukrthika@gmail.com>
2017-06-17 22:45:31 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,37 @@
+var questions = [{
+	"question": "Tension party of this gang",
+	"option1": "vidhya",
+	"option2": "sriram",
+	"option3": "mk",
+	"option4": "tamil",
+	"answer": "2"
+}, {
+	"question": "Cool ice of the gang",
+	"option1": "vidhya",
+	"option2": "ramya",
+	"option3": "krthika",
+	"option4": "tamil",
+	"answer": "1"
+}, {
+	"question": "What is the nickname for Tamil",
+	"option1": "pingu",
+	"option2": "minion",
+	"option3": "tamilz",
+	"option4": "TSK",
+	"answer": "1"
+}, {
+	"question": "Ramya is a state level player in",
+	"option1": "Long jump",
+	"option2": "High jump",
+	"option3": "Sprinter",
+	"option4": "Javilain Throw",
+	"answer": "3"
+}, {
+	"question": "Another name for mk ",
+	"option1": "muthu",
+	"option2": "krthika",
+	"option3": "sowmiya",
+	"option4": "all of the above",
+	"answer": "3"
+
+}];
